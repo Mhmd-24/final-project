ae161dec88f21dfbcfea96cd8abc28fe0d8f373f
Mohammad Abbas <mhmd.abbass379@gmail.com>
2025-04-21 22:33:46 +0300

    Added answer options handeling
    Like styling the correct and incorrect answer and filtering the questions to not repeate the same question twice

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,29 +1,57 @@
 const answerOptions = document.querySelector(".answer-options");
 const nextQuestionBtn = document.querySelector(".next-question-btn");
 let quizCategory = "programming";
+let currentQuestion = null;
+const questionsIndexHistory = [];
 
 const getRandomQuestion = () => {
   const categoryQuestions = questions.find(cat => 
     cat.category.toLowerCase() === quizCategory.toLowerCase()
   )?.questions || [];
 
-  const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)];
+  const availableQuestions = categoryQuestions.filter((_, index) => !questionsIndexHistory.includes(index));
+
+  const randomQuestion = availableQuestions[Math.floor(Math.random() * categoryQuestions.length)];
+  questionsIndexHistory.push(categoryQuestions.indexOf(randomQuestion));
   return randomQuestion;
 };
 
+const highlightCorrectAnswer = () => {
+    const correctOption = answerOptions.querySelectorAll(".answer-option")[currentQuestion.correctAnswer];
+    correctOption.classList.add("correct");
+    const icon = `<i class='bx bx-check-circle'></i>`;
+    correctOption.insertAdjacentHTML("beforeend", icon);
+}
+
+const handleAnswer = (option, answerIndex) => {
+  const isCorrect = currentQuestion.correctAnswer === answerIndex;
+  option.classList.add(isCorrect ? 'correct' : 'incorrect');
+  !isCorrect ? highlightCorrectAnswer() : "";
+
+  const icon = `<i class='bx bx-x-circle'></i>`;
+  option.insertAdjacentHTML("beforeend", icon);
+
+  answerOptions.querySelectorAll(".answer-option").forEach(option => option.style.pointerEvents = "none");
+
+  nextQuestionBtn.style.visibility = "visible";
+}
+
 const renderQuestion = () => {
-  const currentQuestion = getRandomQuestion();
+  currentQuestion = getRandomQuestion();
   if (!currentQuestion) return;
 
   answerOptions.innerHTML = "";
 
   document.querySelector(".question-text").textContent = currentQuestion.question;
 
-  currentQuestion.options.forEach(option => {
+  currentQuestion.options.forEach((option, index) => {
     const li = document.createElement("li");
     li.classList.add("answer-option");
     li.textContent = option;
     answerOptions.appendChild(li);
+    li.addEventListener("click", () => handleAnswer(li, index));
+
+  nextQuestionBtn.style.visibility = "hidden";
   });
 };
 
